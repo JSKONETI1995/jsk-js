5dccb1af4ddd70359037ec13d09e3ac0c8a915b5
JSKONETI1995 <165123853+JSKONETI1995@users.noreply.github.com>
2024-04-15 18:57:03 +0530

    Add files via upload

---
+++ 15.03.24/index.js
@@ -0,0 +1,35 @@
+let number =40;
+if(number%5==0){
+    console.log('the given number is divisible by 5');
+}
+let n=35;
+if(n%2==0)
+{
+    console.log('The given number is Even');
+}
+else{
+    console.log('The given number is odd');
+}
+let item1='biryani';
+let item2='icecream';
+let item3='desert';
+if('item3'){
+    console.log('serving biryani is my love language');
+}
+else if('item1'){
+    console.log('Eating ice cream keeps our relation cool');
+}
+else if('item2'){
+    console.log('Eating desert keeps our talks sweet');
+}
+else{
+    console.log('eating this with you make our date sucessful');
+}
+let s1= true;
+ let s2= true;
+ let and= s1 && s2;
+console.log(true);
+let or= s1||s2;
+console.log(false);
+let not= s1!=s2;
+console.log(false);
