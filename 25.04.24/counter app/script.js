0f909e691a378e1070d2309acb2dbe0517de37a5
JSKONETI1995 <165123853+JSKONETI1995@users.noreply.github.com>
2024-04-26 19:40:51 +0530

    Update script.js

--- 25.04.24/counter app/script.js
+++ 25.04.24/counter app/script.js
@@ -1,38 +1,52 @@
-let value = document.getElementById("value");
-let incrementBtn = document.getElementById("increment");
-let decrementBtn = document.getElementById("decrement");
-let resetBtn = document.getElementById("reset");
-let numbInput = document.getElementById("numb");
-let decBtn = document.getElementById("decup");
-let incBtn = document.getElementById("incup");
+let value = document.getElementById('value');
+let incrementBtn = document.getElementById('increment');
+let decrementBtn= document.getElementById('decrement');
+let resetBtn = document.getElementById('reset');
+let userInput= document.getElementById('input-feild');
+let incrementByUserInput = document.getElementById('incup');
+let decrementByUserInput = document.getElementById('decup');
 
-function updateCounter(newValue) {
-  value.innerHTML = newValue;
+
+incrementByUserInput.addEventListener ('click',()=>{ 
+  let userValue= userInput.value;
+  let convertToNumber = Number(userValue);
+
+  let resultValue = value.textContent;
+  let convertValueToNumber = Number(resultValue);
+  let total =  convertToNumber + convertValueToNumber ;
+  value.innerHTML = total;
+
+
+});
+decrementByUserInput.onclick = () =>{
+  let userValue = userInput.value;
+  let convertToNumber1 = Number(userValue);
+
+  let resultValue = value.textContent;
+  let convertValueToNumber2 = Number(resultValue);
+  let total = convertValueToNumber2 -  convertToNumber1 ;
+  value.innerHTML = total;
+
 }
 
+
+console.log(incrementByUserInput);
+
 incrementBtn.onclick = () => {
-  let currentValue = parseInt(value.innerHTML);
-  updateCounter(currentValue + 1);
-};
+    let convertNumberValue = Number(value.textContent);
+    let addPlusOne = convertNumberValue + 1;
+    value.innerHTML = addPlusOne;
+}
 
 decrementBtn.onclick = () => {
-  let currentValue = parseInt(value.innerHTML);
-  updateCounter(currentValue - 1);
-};
+    let convertNumberValue = Number(value.textContent);
+    let removeOneValue = convertNumberValue - 1;
+    value.innerHTML = removeOneValue ;
+}
 
-resetBtn.onclick = () => {
-  updateCounter(0);
-  numbInput.value = "";
-};
 
-incBtn.onclick = () => {
-  let currentValue = parseInt(value.innerHTML);
-  let inputNumber = parseInt(numbInput.value);
-  updateCounter(currentValue + inputNumber);
-};
+resetBtn.onclick = () => value.innerHTML = 0;
 
-decBtn.onclick = () => {
-  let currentValue = parseInt(value.innerHTML);
-  let inputNumber = parseInt(numbInput.value);
-  updateCounter(currentValue - inputNumber);
-};
+
+
+
